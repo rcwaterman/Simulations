67f20b078d534ac095cf2d52ee8f9c1a270f0992
rcwaterman <134216651+rcwaterman@users.noreply.github.com>
2024-03-24 14:39:42 +0000

    modified:   ball.py

--- BouncingBall/ball.py
+++ BouncingBall/ball.py
@@ -1,2 +1,31 @@
-import tkinter as tk
-import PIL
+import pygame
+from pygame.locals import *
+import os
+os.environ['SDL_AUDIODRIVER'] = 'directx'
+
+"""
+TODO:
+
+1. Add a pygame window
+    a. Make the window size dynamic (use a general purpose "scale" variable)
+2. Draw a bounding box and a ball within it.
+3. Add physics (gravity, coefficient of restitution, collision detection, change in direction, etc.)
+4. Determine what to do upon collision (increase velocity, change ball color, trace ball motion, make sound, etc)
+
+"""
+
+pygame.init()
+
+#MAKE A SCREEN FOR THE PLAYER TO SEE
+screen = pygame.display.set_mode((800, 400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.fill((255,255,255))
+
+    pygame.display.update()
+
+pygame.quit()
+quit()
