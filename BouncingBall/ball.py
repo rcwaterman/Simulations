c95fd022dfd1e37cc84a88ad60a2a48bdc7bd106
Ryan <watermanrw@gmail.com>
2024-03-25 03:55:03 -0400

    modified:   ball.py

--- BouncingBall/ball.py
+++ BouncingBall/ball.py
@@ -22,10 +22,13 @@ screen = pygame.display.set_mode((dims.current_w*0.9, dims.current_h*0.9))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-gravity = 1.05
-v_init_x = 0.5
-v_init_y = 0
+color = (255, 255, 255)
+cor = 0.98
+gravity = 9.8
+v_x = 10
+v_y = 0
 radius = 20
+contact_count = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -39,19 +42,24 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    pygame.draw.circle(screen, "white", player_pos, radius)
+    pygame.draw.circle(screen, color, player_pos, radius)
 
     player_pos.y+=gravity
-    player_pos.x+=v_init_x
-    gravity+=0.05
+    player_pos.x+=v_x
     
     # reverse direction if the ball hits either y bounding box
     if math.floor(player_pos.y + radius) >= screen.get_height() or math.floor(player_pos.y - radius) <= 0:
-        gravity = gravity*-1
+        gravity = gravity*-1*cor
+        v_x = v_x*cor
+        contact_count+=1
 
     # reverse direction if the ball hits either x bounding box
-    if math.floor(player_pos.y + radius) >= screen.get_height() or math.floor(player_pos.y - radius) <= 0:
-        gravity = gravity*-1
+    if math.floor(player_pos.x + radius) >= screen.get_width() or math.floor(player_pos.x - radius) <= 0:
+        v_x = v_x*-1*cor
+        gravity = gravity*cor
+        contact_count+=1
+
+    gravity+=1
 
     # flip() the display to put your work on screen
     pygame.display.flip()
